9b93bddf954b77d67567b4649209f7c5ef5d1540
dikeshchouha1 <dikeshchouhan02@gmail.com>
2025-12-22 13:45:53 +0530

    models create

---
+++ BlogApplication/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SyncoSyntax.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public int Id { get; set; }
+
+        public string  Name { get; set; }
+
+        public string? Description { get; set; }
+
+
+        public ICollection<Post> Posts { get; set; }
+
+
+    }
+}
---
+++ BlogApplication/Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SyncoSyntax.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "UserName is required")]
+        [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        public string CommentDate { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [ForeignKey("Post")]
+        public int PostId { get; set; }
+
+        public Post Post { get; set; }
+
+    }
+}
