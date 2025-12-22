62d3f65db26845df129072c92a29c151731b406d
dikeshchouha1 <dikeshchouhan02@gmail.com>
2025-12-22 17:45:35 +0530

    Dbcontext DBconnection

---
+++ BlogApplication/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyncoSyntax.Models;
+
+namespace BlogApplication.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
